7b8043d8f2a842ba0608459c7458119770035573
root <kidusasebe1919@gmail.com>
2024-01-10 12:26:23 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""0-lookup.py is a module that contains the lookup function."""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return dir(obj)
