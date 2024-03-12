dac7a36d9c338321534687a3911d386cb92f7181
Kad-19 <kidusasebe1919@gmail.com>
2024-03-12 09:53:21 -0700

    correct comments on all files

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
 -- a script that deletes the database hbtn_0c_0 in your MySQL server
---If the database hbtn_0c_0 doesn’t exist, the script will not fail
+-- If the database hbtn_0c_0 doesn’t exist, the script will not fail
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
